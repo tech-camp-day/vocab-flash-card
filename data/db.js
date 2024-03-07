1d919ea934819a82219c5485cbbc3e4064d3229d
Plai <plai.rt.w@gmail.com>
2024-03-07 19:36:26 +0000

    Add table and feature skeleton

--- data/db.js
+++ data/db.js
@@ -6,19 +6,30 @@ const db = require('better-sqlite3')('vocab-flash-card.db', { verbose: console.l
 const initDb = () => {
     const createVocabTable = db.prepare(`
       create table if not exists vocabs (
-      
+        id integer primary key autoincrement,
+        word text not null,
+        meaning text not null
       )
     `);
 
     const createUserTable = db.prepare(`
       create table if not exists user (
-
+        lineUserId text primary key,
+        currentVocabId integer,
+        currentAnswered boolean default false,
+        currentCorrect boolean default false,
+        score integer default 0
       )
     `);
 
     const createUserHistoryTable = db.prepare(`
       create table if not exists userHistory (
-
+        lineUserId text,
+        vocabId integer,
+        correct boolean,
+        createdDate integer default current_timestamp,
+        foreign key (lineUserId) references user(lineUserId),
+        foreign key (vocabId) references vocabs(id)
       )
     `)
 
--- line/messageHandler.js
+++ line/messageHandler.js
@@ -1,12 +1,44 @@
+const { reply } = require('./messageSender');
+
 /**
  * จัดการกับเหตุการณ์ที่เข้ามา
  * @param {object} event - อ็อบเจ็กต์เหตุการณ์
  */
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return;
+  switch (event.type) {
+    case 'follow':
+      handleFollow(event);
+      break;
+    case 'unfollow':
+      handleUnfollow(event);
+      break;
+    case 'message':
+      handleMessage(event);
+      break;
+    default:
+      break;
+}
+
+function handleFollow(event) {
+  reply(event, 'สวัสดีครับ มาเล่นทายคำกันเถอะ! จะมีศัพท์ภาษาอังกฤษส่งให้ทุกวันตอน 6 โมงเย็น แต่ถ้าจะเล่นตอนนี้พิมพ์ว่า "ขอศัพท์" ได้เลยครับ');
+  // todo create user 
+}
+
+function handleUnfollow(event) {
+  console.log(JSON.stringify(event, null, 2));
+  // todo delete user and history
+}
+
+function handleMessage(event) {
+  if (event.message.type !== 'text') {
+    reply('ส่งสติ้กเกอร์มาเพื่อ?');
   }
 
+  if (event.message.text === 'ขอศัพท์') {
+    // todo send word
+  }
+
+  // todo check answer, tell user to wait for tommorow, update score
 }
 
 module.exports = { handleEvent };
