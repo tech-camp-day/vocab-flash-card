c956e3b27276b36743f7ca5b2f8b0837758741ea
Plai <plai.rt.w@gmail.com>
2024-09-09 13:14:06 +0000

    be more specific on question and answer placeholders

--- src/line/message.js
+++ src/line/message.js
@@ -6,14 +6,14 @@ const MESSAGE_LANG = {
     NOT_TEXT: 'ส่งสติ้กเกอร์มาเพื่อ?',
     START_ERROR: 'มีข้อผิดพลาดในการเริ่มต้น กรุณาลอง block แล้ว unblock บอทใหม่อีกครั้ง',
     GIVE_WORD: 'ขอศัพท์',
-    ASK_WORD: 'คำว่า "{word}" แปลว่าอะไร?',
+    ASK_WORD: 'คำว่า "{question}" แปลว่าอะไร?',
     SEE_SCORE: 'ดูคะแนน',
     STAT: 'คุณตอบถูก {correct}/{total} ครั้งใน 7 วันที่ผ่านมา',
     NO_ASSIGNED_WORD: "ยังไม่มีศัพท์ให้ทายครับ",
     HELP: 'ถ้าอยากเล่นตอนนี้พิมพ์ว่า "ขอศัพท์" หรือถ้าต้องการดูคะแนนพิมพ์ว่า "ดูคะแนน" ได้เลยครับ',
     CORRECT: 'ถูกต้อง',
     WRONG: 'ผิด',
-    SHOW_ANSWER: 'คำว่า "{word}" แปลว่า "{meaning}"',
+    SHOW_ANSWER: 'คำว่า "{question}" แปลว่า "{answer}"',
     REPORT: 'สรุปคะแนนสัปดาห์ที่ผ่านมาของคุณ: {correct}/{total} คะแนน'
   },
   en: {
@@ -23,14 +23,14 @@ But if you want to play now, type "Give me a word"`,
     NOT_TEXT: 'Why send a sticker?',
     START_ERROR: 'There was an error starting. Please try blocking and unblocking me again',
     GIVE_WORD: 'Give me a word',
-    ASK_WORD: 'What does "{word}" mean?',
+    ASK_WORD: 'What does "{question}" mean?',
     SEE_SCORE: 'See score',
     STAT: 'You answered {correct}/{total} correctly in the past 7 days',
     NO_ASSIGNED_WORD: "There is no word to guess yet",
     HELP: 'If you want to play now, type "Give me a word" or if you want to see the score, type "See score"',
     CORRECT: 'Correct',
     WRONG: 'Wrong',
-    SHOW_ANSWER: 'The word "{word}" means "{meaning}"',
+    SHOW_ANSWER: 'The word "{question}" means "{answer}"',
     REPORT: 'Your weekly score summary: {correct}/{total} points'
   }
 };
